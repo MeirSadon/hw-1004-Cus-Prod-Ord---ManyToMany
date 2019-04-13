62d91abcf175c0dceeced28c80bbab1822768f53
MeirSadon <45739995+MeirSadon@users.noreply.github.com>
2019-04-13 22:16:05 +0300

    Update hw ManyToMany.sql

--- hw ManyToMany.sql
+++ hw ManyToMany.sql
@@ -8,3 +8,13 @@ select name from (select name, max(sop) from (SELECT c.name, sum(price) as sop f
 select name from (SELECT c.name, count(*) as counter from Customer c JOIN ORDERS o on c.id!=o.CUSTOMER_ID  GROUP by c.id having counter= (select count(*) from ORDERS))
                                                                                                                                           
                                                                                                                                           
+//מצא הזמנות שגויות
+                                                                                                                                          
+                                                                                                                                          
+ //מצא סכום כולל של הזמנות לפי לקוח
+select c.name as CusName, sum(price) as PRICE from ORDERS o join Customer c on c.id=o.CUSTOMER_ID join Products p on o.PRODUCT_ID=p.id group by cusname
+                                                                                                                                          
+//ממוצע ההזמנות של כל לקוח
+select  c.name as cusName, avg(price) as avger from ORDERS o join Customer c on c.id=o.CUSTOMER_ID join Products p on o.PRODUCT_ID=p.id group by c.name
+                                                                                                                                          
+/לקוחות עם מעל הממוצע של ההזמנות
